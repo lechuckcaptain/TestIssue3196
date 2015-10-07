6bfaf9847c85a2fcd70e98b10de924c74af1fee0
lechuckcaptain <lechuckcaptain@gmail.com>
2015-10-07 14:48:48 +0200

    Created repository

---
+++ app/src/main/java/test/devpreview/com/testissue3196/MainActivity.java
@@ -0,0 +1,129 @@
+package test.devpreview.com.testissue3196;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
+import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+{
+    private WifiManager mWifiManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings)
+        {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void testIssue(View view)
+    {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_WIFI_STATE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_WIFI_STATE}, 1);
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CHANGE_WIFI_STATE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CHANGE_WIFI_STATE}, 2);
+        }
+
+        mWifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
+        List<WifiConfiguration> configuredNetworks = mWifiManager.getConfiguredNetworks();
+
+        if (configuredNetworks.size() > 0)
+        {
+            for(WifiConfiguration configuration : configuredNetworks)
+            {
+                configuration.hiddenSSID = true;
+                configuration.providerFriendlyName = "ABCD";
+                int result = mWifiManager.updateNetwork(configuration);
+
+                if (result == -1)
+                {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                    builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                        @Override
+                        public void onClick(DialogInterface dialogInterface, int i)
+                        {
+                            dialogInterface.dismiss();
+                        }
+
+                    });
+                    builder.setTitle("ERROR");
+                    builder.setMessage(String.format("Received -1 result updating Wi-Fi network %s, please check Logcat for further information", configuration.SSID));
+                    builder.show();
+                }
+            }
+        }
+        else
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i)
+                {
+                    dialogInterface.dismiss();
+                }
+
+            });
+            builder.setTitle("ERROR");
+            builder.setMessage("Cannot find available Wi-Fi networks to update");
+            builder.show();
+        }
+    }
+
+
+    public static SecurityType getSecurity(WifiConfiguration config)
+    {
+        if (config.allowedKeyManagement.get(WifiConfiguration.KeyMgmt.WPA_PSK))
+        {
+            return SecurityType.SECURITY_PSK;
+        }
+        if (config.allowedKeyManagement.get(WifiConfiguration.KeyMgmt.WPA_EAP) ||
+                config.allowedKeyManagement.get(WifiConfiguration.KeyMgmt.IEEE8021X))
+        {
+            return SecurityType.SECURITY_EAP;
+        }
+        return (config.wepKeys[0] != null) ? SecurityType.SECURITY_WEP : SecurityType.SECURITY_NONE;
+    }
+}
